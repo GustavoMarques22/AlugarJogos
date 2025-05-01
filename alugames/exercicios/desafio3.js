6a2bea41df37b629043e9709b54824a85b45450b
Gustavo <gustavo.marques@aluno.unievangelica.edu.br>
2025-05-01 18:22:47 -0300

    Add files via upload

---
+++ alugames/exercicios/desafio3.js
@@ -0,0 +1,14 @@
+/*Crie um programa que verifica se uma palavra ou frase é um palíndromo.*/
+sssaPalavraEUmPalindromo();
+
+function essaPalavraEUmPalindromo(){
+    var palavra = "rever";
+    var separandoAsLetrasDaPalavra = palavra.split("");
+    var palavraInvertida = separandoAsLetrasDaPalavra.reverse;
+    palavraInvertida = palavraInvertida.join("");
+    if(palavra == palavraInvertida){
+        console.log("A palavra " + palavra + " é um palíndromo!");
+    } else{
+        console.log("A palavra " + palavra + " não é um palíndromo!");
+    }
+}
---
+++ alugames/exercicios/desafio4.js
@@ -0,0 +1,8 @@
+/*Crie um programa com uma função que receba três números como argumentos e os retorne em ordem crescente. Exiba os números ordenados.*/
+function ordenarNumeros(a, b, c) {
+    const numerosOrdenados = [a, b, c].sort((x, y) => x - y);
+    console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
+}
+
+// Exemplo de uso:
+ordenarNumeros(3, 1, 5); // Deve exibir "Números ordenados: 1, 3, 5"
